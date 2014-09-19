2b1efcd6065e70aca4f5d3432690d736fc3df73d
Christopher Stingl <cjstingl@gmail.com>
2014-09-19 14:14:53 -0400

    Add explicit file names

--- package.js
+++ package.js
@@ -1,5 +1,5 @@
 Package.describe({
-  name: "wolves:bitters"
+  name: "wolves:bitters",
   summary: "Meteor 0.9.x - Scaffold styles, variables and structure for Bourbon projects.",
   version: "0.1.0",
   git: "https://github.com/wolvesio/meteor-bitters"
@@ -9,21 +9,21 @@ Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.0');
   api.use('fourseven:scss@0.9.5');
   api.addFiles([
-    "base.scss",
+    "_base.scss",
     // Variables
-    "variables.scss",
+    "_variables.scss",
     // Extends
-    "extends/button.scss",
-    "extends/clearfix.scss",
-    "extends/errors.scss",
-    "extends/flashes.scss",
-    "extends/hide-text.scss",
+    "extends/_button.scss",
+    "extends/_clearfix.scss",
+    "extends/_errors.scss",
+    "extends/_flashes.scss",
+    "extends/_hide-text.scss",
     // Typography and Elements
-    "typography.scss",
-    "forms.scss",
-    "tables.scss",
-    "lists.scss",
-    "buttons.scss"
+    "_typography.scss",
+    "_forms.scss",
+    "_tables.scss",
+    "_lists.scss",
+    "_buttons.scss"
   ], 'server', {
     isAsset: true
   });
